372f9f4a861c28cb1c66aa3748705a5011a173a9
eoekstpal <park.se.mi47@gmail.com>
2024-09-19 01:12:01 +0900

    [create]: randingpage 추가, 기본틀

---
+++ src/views/RandingView.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="w-full h-full">
+        <header class="w-full h-screen bg-[#031F34]">
+            <section class="text-white">
+                로고
+            </section>
+            <section>
+                <p>문코에서</p>
+                <h1>세상에 버려질 <br/>
+                음식 구출하기</h1>
+            </section>
+            <div>
+                <button>문코 시작하기</button>
+            </div>
+        </header>
+
+        <main class="bg-[#F2F2F2] px-[242px]">
+            <div class="">
+                문코여야 하는 이유
+            </div>
+            <section class="flex">
+                <div> 카드</div>
+                <div>
+                    <p>가치 소비로 환경 지키기</p>
+                    <p>오늘이 지나면 버려지는 음식을 구매하고 <br/>
+                    음식물 쓰레기를 줄여 환경을 지키세요</p>
+                </div>
+            </section>
+            <section>
+                <div>
+                    <p>가치 소비로 환경 지키기</p>
+                    <p>오늘이 지나면 버려지는 음식을 구매하고 <br/>
+                    음식물 쓰레기를 줄여 환경을 지키세요</p>
+                </div>
+                <div> 카드</div>
+            </section>
+            <section>
+                <div> 카드</div>
+                <div>
+                    <p>가치 소비로 환경 지키기</p>
+                    <p>오늘이 지나면 버려지는 음식을 구매하고 <br/>
+                    음식물 쓰레기를 줄여 환경을 지키세요</p>
+                </div>
+            </section>
+        </main>
+
+            <article>
+                <section>
+                    <div>이미지</div>
+                    <div>
+                        <p>마감할인으로<br/>
+                        음식 구매하기</p>
+                        <p>문코에서 음식을 저렴하게 구매하고<br/>
+                        환경과 지갑을 지키세요</p>
+                    </div>
+                </section>
+                <section>
+                    <div>이미지</div>
+                    <div>
+                        <p>마감할인으로<br/>
+                        음식 구매하기</p>
+                        <p>문코에서 음식을 저렴하게 구매하고<br/>
+                        환경과 지갑을 지키세요</p>
+                    </div>
+                </section>
+                <section>
+                    <div>이미지</div>
+                    <div>
+                        <p>마감할인으로<br/>
+                        음식 구매하기</p>
+                        <p>문코에서 음식을 저렴하게 구매하고<br/>
+                        환경과 지갑을 지키세요</p>
+                    </div>
+                </section>
+                <div>
+                    <p>문코에서 세상에 버려질 음식 구출하기</p>
+                    <button>문코 시작하기</button>
+                </div>
+            </article>
+
+            <footer>
+                로고
+            </footer>
+    </div>
+</template>
